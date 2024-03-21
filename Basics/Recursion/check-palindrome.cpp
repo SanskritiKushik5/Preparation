5c76afc0cb9a225259bafffc364d29e3e1a89da0
Sanskriti Kushik <sanskritikushik23@gmail.com>
2024-03-21 18:57:01 +0530

    Palindrome

---
+++ Basics/Recursion/check-palindrome.cpp
@@ -0,0 +1,11 @@
+int check(int l, int r, string& str){
+    if(l>=r) return 1;
+    if(str[l]!=str[r]) return 0;
+    return check(l+1, r-1, str);
+}
+
+bool isPalindrome(string& str) {
+    int n = str.length();
+    if (!check(0, n-1, str)) return false;
+    return true;
+}
